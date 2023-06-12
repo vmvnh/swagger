06a48c62f98debda56de1631e474c48a92609a65
Yaroslav <yaroslav-w@mail.ru>
2023-06-12 12:44:14 +0400

    initialisation

---
+++ src/main/java/com/test/swagger/OpenApiConfig.java
@@ -0,0 +1,7 @@
+//package com.test.swagger;
+//
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.info.Info;
+//
+//@OpenAPIDefinition(info = @Info(title = "MicroApp", description = "Тестовая реализация микросервисов"))
+//public class OpenApiConfig {}
